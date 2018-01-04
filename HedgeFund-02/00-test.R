2956117d0b17d377514f28c4649eafef7dfe5155
Ross Zhu <ross.zhu@outlook.com>
2018-01-04 11:23:56 -0700

    针对第二次revision的修改，增加market volatility

--- HedgeFund-02/00-test.R
+++ HedgeFund-02/00-test.R
@@ -1,5 +1,29 @@
-ld(r.manager)
-ld(r.manager.resume)
-ld(r.manager.mapping)
+garch.sigma <- function(x) {
+    spec <- ugarchspec(distribution.model = "std")
+    fit <- ugarchfit(spec, x, solver = 'hybrid') %>% ugarchforecast(n.ahead = 1)
+    fit@forecast$sigmaFor[1, 1]
+}
 
-manager <- r.manager[r.manager.resume, on = .(user_id), nomatch = 0]
+system.time({
+roll.dopar <- hs300[1:180, {
+    n <- 120
+    garch.sigma <- function(x) {
+    spec <- ugarchspec(distribution.model = "std")
+    fit <- ugarchfit(spec, x, solver = 'hybrid') %>% ugarchforecast(n.ahead = 1)
+    fit@forecast$sigmaFor[1, 1]
+    }
+
+    foreach(t = (n + 1):.N, .final = rbindlist) %dopar% { 
+        skew <- skewness(dret[(t - n):t])
+        rv <- sum((dret[(t - n):t]) ^ 2)
+        gv <- garch.sigma(dret[(t - n):t])
+        list(date = date[t], skew = skew, rv = rv, gv = gv)
+    }
+    }
+]
+})
+
+
+
+
+identical(roll.lapply, roll.for)
